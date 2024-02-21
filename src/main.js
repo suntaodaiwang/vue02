c7a04437fdb258251d28d2f94c1457570e584b58
2539871881@qq.com <2539871881@qq.com>
2024-02-21 10:20:41 +0800

    vue02

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui';// 添加
+import 'element-ui/lib/theme-chalk/index.css'; // 添加
 import App from './App.vue'
 import router from './router'
-import store from './store'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false // 添加 取消生产提示
+Vue.use(ElementUI);// 添加
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+ router,
+ render: h => h(App)
 }).$mount('#app')
+
